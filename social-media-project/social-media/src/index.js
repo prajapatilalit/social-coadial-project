12b7bc62822ecdd6d4ecd84cf354717a5eb3e315
prajapatilalit <prajapatilalit150992@gmail.com>
2020-10-02 13:22:34 +0530

    configure the store

---
+++ social-media-project/social-media/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { configureStore } from './store';
+
+const store = configureStore();
+console.log('store', store.getState());
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
